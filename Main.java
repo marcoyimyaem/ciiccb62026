5a607e3274ecb507c8bfc75b3646d2b415dd8949
marcoyimyaem <marcoyimyaem@gmail.com>
2026-05-18 08:01:20 +0000

    Day

--- Main.java
+++ Main.java
@@ -3,5 +3,14 @@ class Main{
         System.out.println("Hello World");//codespace
         System.out.println("Hello World2");//vscode local pc
         System.out.println("Hello World3");//codespace
+        Toy toy1 = new Toy();
+        toy1.name = "Rage Pink";
+        toy1.brand = "Lab Vuvu";
+        toy1.price = 4500;
+        toy1.quantity = 12;
+        toy1.setPrice(toy1.price*0.5);//this set the price to 50% for the event sale 6-6-2026
+        System.out.println(toy1.price);
+        Toy toy2 = new Toy();
+        System.out.println("end of program");
     }
 }
---
+++ Toy.java
@@ -0,0 +1,29 @@
+public class Toy {
+    String name;
+    String brand;
+    double price;
+    int quantity;
+    char size;
+    // set the Toy object price;
+    public void setPrice(double price) {
+        this.price = price;
+    }
+/* this is a sample 
+* program if
+* Toy class 
+* is run
+*
+*/
+
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+        toy1.name = "Rage Pink";
+        toy1.brand = "Lab Vuvu";
+        toy1.price = 4500;
+        toy1.quantity = 12;
+        toy1.setPrice(toy1.price*0.5);
+        System.out.println(toy1.price);
+        
+    }
+
+}
