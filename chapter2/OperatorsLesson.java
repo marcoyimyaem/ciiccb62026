27e268e04f28c44e32381076944ce9ced5ffae8c
marcoyimyaem <marcoyimyaem@gmail.com>
2026-05-25 07:59:21 +0000

    Day 11

---
+++ chapter2/OperatorsLesson.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class OperatorsLesson {
+    public static void main(String[] args) {
+        int a = +10;
+        int b= -(5);
+        boolean isActive = false;
+//      ++b++ or --a-- ❌
+        // a= +a++; ✅
+        System.out.println(--a+ ++a); //9+ 10
+        System.out.println(a);
+        System.out.println(-b);
+        System.out.println(!!!!!isActive);
+        System.out.println(5/2);
+        System.out.println(5%2);
+    }
+
+}
