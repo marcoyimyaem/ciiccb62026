1c486a03a182f90c2b37fbf9e7079a40e8d69a2d
marcoyimyaem <marcoyimyaem@gmail.com>
2026-05-26 07:59:49 +0000

    Day 12

--- chapter2/OperatorsLesson.java
+++ chapter2/OperatorsLesson.java
@@ -13,6 +13,32 @@ public class OperatorsLesson {
         System.out.println(!!!!!isActive);
         System.out.println(5/2);
         System.out.println(5%2);
+        System.out.println("3<<2="+(3<<2));
+        //0  0   0  0   1   1   0   0
+        //              8   4   2   1
+        //              1   1   0   0 = 12
+        
+        System.out.println("18>>3="+(18>>3));
+        //0  0   0  1   0   0   1   0
+        //         16   8   4   2   1 
+        
+        //0  1   0   0  0   0   1   0 = 2
+        System.out.println("5>>>3="+(5>>>3));
+        System.out.println(5/5.0); //5-> 5.0 -> 5.0/5.0
+        int g = (int)3.0;
+        short x = 10;
+        short y = 5;
+        short z = (short)(x*y);
+
+        long x2 =10;
+        int y2 = 5;
+        y2 *= x2; // y2 =(type y2)(y2*x2);
+        boolean isFailed = false, isFailed3;
+        boolean isFailed2=isFailed3=isFailed=true; 
+        System.out.println(isFailed);
+        System.out.println(isFailed2);
+        System.out.println(isFailed3);
+
     }
 
 }
