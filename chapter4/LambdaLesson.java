7763cdcad986b1cf59ab68182045c930841f2393
marcoyimyaem <marcoyimyaem@gmail.com>
2026-06-13 19:00:25 +0800

    Update LambdaLesson.java

--- chapter4/LambdaLesson.java
+++ chapter4/LambdaLesson.java
@@ -2,9 +2,10 @@ package chapter4;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class LambdaLesson {
-    static void print(List<Animal> animals, CheckTrait checker){
+    static void print(List<Animal> animals, Predicate<Animal> checker){
         for(Animal animal:animals){
             if(checker.test(animal)) System.out.print(animal+", ");
         
@@ -13,19 +14,35 @@ public class LambdaLesson {
 
     }
     public static void main(String[] args) {
-    List<Animal> animals = new ArrayList<>();
-    animals.add(new Animal("fish", false, true));
-    animals.add(new Animal("kangaroo", true, false));
-    animals.add(new Animal("rabbit", true, false));
-    animals.add(new Animal("turtle", false, true));
-    // print(animals, new CheckIfHopper());
-    // System.out.println("check if swimmer");
-    // print(animals, new CheckIfSwimmer());
-    System.out.println("can hop lambda");
-    print(animals, a->a.canHop());
-    System.out.println("can swim lambda");
-    print(animals, a->a.canSwim());
-
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("fish", false, true,1));
+        animals.add(new Animal("kangaroo", true, false,5));
+        animals.add(new Animal("rabbit", true, false,2));
+        animals.add(new Animal("turtle", false, true,56));
+        animals.add(new Animal("camel", false, false,12));
+        animals.add(new Animal("frog", true, true,1));
+        animals.add(new Animal("dog", true, true,7));
+        // print(animals, new CheckIfHopper());
+        // System.out.println("check if swimmer");
+        // print(animals, new CheckIfSwimmer());
+        System.out.println("can hop lambda");
+        print(animals, a->a.canHop());
+        System.out.println("can swim lambda");
+        print(animals, a->a.canSwim());
+        print(animals, a->!a.canSwim()&& !a.canHop());
+        print(animals, a->a.canSwim()&& a.canHop());
+        print(animals, a->a.age>5);
+        TwoNumberOps addTwoNums = (a,b,c)-> a*b+1000/6;
+        TwoNumberOps mulTwoNums = (p,o,l)-> p*p+o;
+        System.out.println(addTwoNums.aOpsB(6, 7,true));
+        System.out.println(mulTwoNums.aOpsB(6,9, false));
+        DiscountItem tenPercent = (a)-> a-(a*0.10);
+        DiscountItem twentyPercent = (a)-> a-(a*0.20);
+        DiscountItem fiftyPercent = (a)-> a-(a*0.50);
+        double price = 1000;
+        System.out.println(tenPercent.dc(price));
+        System.out.println(twentyPercent.dc(price));
+        System.out.println(fiftyPercent.dc(price));
 }
 }
 
@@ -33,11 +50,12 @@ class Animal{
     String spicies;
     boolean canHop;
     boolean canSwim;
-
-    public Animal(String spicies, boolean canHop, boolean canSwim){
+    int age;
+    public Animal(String spicies, boolean canHop, boolean canSwim, int age){
         this.spicies=spicies;
         this.canHop=canHop;
         this.canSwim=canSwim;
+        this.age= age;
     }
     boolean canHop(){
         return canHop;
@@ -51,9 +69,9 @@ class Animal{
         return spicies;
     }
 }
-interface CheckTrait {
-    boolean test(Animal a);
-}
+// interface CheckTrait {
+//     boolean test(Animal a);
+// }
 // class CheckIfHopper implements CheckTrait{
 
 //     @Override
@@ -70,3 +88,10 @@ interface CheckTrait {
 //     }
 
 // }
+interface TwoNumberOps{
+    float aOpsB(float a, float b,boolean c);
+}
+
+interface DiscountItem{
+    double dc(double b);
+}
